cd7525c46fba4fb52fb31795256d6a562b1d4abc
Batel Cohen <93344134+BatelCohen7@users.noreply.github.com>
2023-06-16 15:41:06 +0300

    Delete sources directory

--- sources/TrainedNinja.cpp
+++
@@ -1,6 +0,0 @@
-#include "TrainedNinja.hpp"
-
-using namespace std;
-using namespace ariel;
-
-TrainedNinja::TrainedNinja(string name, const Point &location): Ninja(name, const_cast<Point&>(location), 150, 8) {}
--- sources/YoungNinja.cpp
+++
@@ -1,6 +0,0 @@
-#include "YoungNinja.hpp"
-
-using namespace std;
-using namespace ariel;
-
-YoungNinja::YoungNinja(string name, const Point &location): Ninja(name, const_cast<Point&>(location), 150, 8) {}
